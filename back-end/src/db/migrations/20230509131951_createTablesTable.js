3b073dcc08de50c4b7c535397deea27869398a8a
Matthew Murray <matthewc.murray94@gmail.com>
2023-05-16 13:43:14 -0500

    minor formatting

--- back-end/src/db/migrations/20230509131951_createTablesTable.js
+++ back-end/src/db/migrations/20230509131951_createTablesTable.js
@@ -1,15 +1,14 @@
-
-exports.up = function(knex) {
-    return knex.schema.createTable("tables", (table) => {
-        table.increments("table_id").primary();
-        table.string("table_name").notNullable();
-        table.integer("capacity",null).unsigned().notNullable();
-        table.integer('reservation_id').unsigned().nullable();
-        table.foreign('reservation_id').references('reservations.reservation_id')
-        table.timestamps(true, true);
-    })
+exports.up = function (knex) {
+  return knex.schema.createTable("tables", (table) => {
+    table.increments("table_id").primary();
+    table.string("table_name").notNullable();
+    table.integer("capacity", null).unsigned().notNullable();
+    table.integer("reservation_id").unsigned().nullable();
+    table.foreign("reservation_id").references("reservations.reservation_id");
+    table.timestamps(true, true);
+  });
 };
 
-exports.down = function(knex) {
-    return knex.schema.dropTable("tables");
+exports.down = function (knex) {
+  return knex.schema.dropTable("tables");
 };
--- back-end/src/db/seeds/00-tables.js
+++ back-end/src/db/seeds/00-tables.js
@@ -1,5 +1,9 @@
-const tables = require("./00-tables.json")
+const tables = require("./00-tables.json");
 
 exports.seed = function (knex) {
-  return knex.raw("TRUNCATE TABLE tables RESTART IDENTITY CASCADE").then(function () {return knex("tables").insert(tables)});
+  return knex
+    .raw("TRUNCATE TABLE tables RESTART IDENTITY CASCADE")
+    .then(function () {
+      return knex("tables").insert(tables);
+    });
 };
